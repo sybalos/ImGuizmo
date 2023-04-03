4e7ed886cf627242011c4c472d6ed5ef06018c8b
sybalos <sybalos@gmail.com>
2023-04-03 08:01:30 -0400

    Renamed and sharpmake

---
+++ imguizmogen.cs
@@ -0,0 +1,45 @@
+using Sharpmake;
+using System.IO;
+
+[Generate]
+public class ImGuizmo : Project
+{
+    public ImGuizmo()
+    {
+        Name = "ImGuizmo";
+        SourceRootPath = @"[project.SharpmakeCsPath]\src";
+        AddTargets(new Target(
+            Platform.win32 | Platform.win64,
+            DevEnv.vs2022,
+            Optimization.Debug | Optimization.Release,
+            OutputType.Dll | OutputType.Lib
+        ));
+    }
+
+    [Configure]
+    public void ConfigureAll(Project.Configuration conf, Target target)
+    {
+        conf.IncludePaths.Add( @"C:\repos\impdev\imp\source\imgui");
+        // This is the name of the configuration. By default, it is set to
+        // [target.Optimization] (so Debug or Release), but both the debug and
+        // release configurations have both a shared and a static version so
+        // that would not create unique configuration names.
+        conf.Name = @"[target.Optimization] [target.OutputType]";
+
+        // Gives a unique path for the project because Visual Studio does not
+        // like shared intermediate directories.
+        conf.ProjectPath = Path.Combine("[project.SharpmakeCsPath]/projects/[project.Name]");
+
+        // Sets the include path of the library. Those will be shared with any
+        // project that adds this one as a dependency. (The executable here.)
+        conf.IncludePaths.Add(@"[project.SourceRootPath]");
+
+        // The library wants LIBRARY_COMPILE defined when it compiles the
+        // library, so that it knows whether it must use dllexport or
+        // dllimport.
+        //conf.Defines.Add("YAML_CPP_STATIC_DEFINE");
+
+        // We want this to output a static library. (LIB)
+        conf.Output = Configuration.OutputType.Lib;
+    }
+}
